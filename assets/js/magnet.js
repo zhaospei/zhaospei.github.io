26c1aa05c92e847da7d133fa90397e3769fd9dd7
zhaospei <dungbuit1k28@gmail.com>
2023-02-21 03:27:10 +0700

    update maget position edge

--- assets/js/magnet.js
+++ assets/js/magnet.js
@@ -4,8 +4,20 @@ $(".magnet").each(function () {
 
 function dragEnd(event) {
   var leftextra = 0.1 * window.innerWidth / 2;
-  event.target.style.top = event.clientY - 60 + "px";
-  event.target.style.left = event.clientX - leftextra + "px";
+  if (event.clientY - 60 > 200) {
+    event.target.style.top = "200px";
+  } else if (event.clientY < 50) {
+    event.target.style.top = "0px";
+  } else {
+    event.target.style.top = event.clientY - 60 + "px";
+  }
+  var newLeft = $(window).width() - 2 * parseInt(event.target.style.width, 10) - 10;
+  console.log(newLeft);
+  if (event.clientX - leftextra > newLeft) {
+    event.target.style.left = newLeft + "px";
+  } else {
+    event.target.style.left = event.clientX - leftextra + "px";
+  }
   event.target.style.transform = "rotate(".concat(Math.floor(20 * Math.random()) - 10, "deg)");
 }
 
