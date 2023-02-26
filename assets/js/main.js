00de23d87ed96f23bb008e3db37221214f16c6f1
zhaospei <dungbuit1k28@gmail.com>
2023-02-26 17:33:19 +0700

    make a link open more clearly

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 var body = document.body,
     html = document.documentElement;
 
-var height = Math.max( body.scrollHeight, body.offsetHeight, 
-                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+var height = Math.max(body.scrollHeight, body.offsetHeight,
+    html.clientHeight, html.scrollHeight, html.offsetHeight);
 $('#particles-js').css('height', height);
 
-$("a").attr("target", "_blank");
+$('.post a').attr('target', '_blank');
+
+$('.project a').attr('target', '_blank');
