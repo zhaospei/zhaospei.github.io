a2feb27cd1a58684d90a710baafb7e2509be5c4a
zhaospei <zhaospei@users.noreply.github.com>
2023-05-02 16:03:14 +0000

    deploy: 4f5c2774ec344ab44d0a8e38f946a812bfbd150a

---
+++ assets/js/clip-text.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".clip-text", {
+    strings: ["Computer Science Student at <span class='font-semibold'>UET-VNU.</span>", 
+    "I live in <span class='font-semibold'>Ha Noi, Viet Nam.</span>", 
+    "I was born in the beautiful <span class='font-semibold'>Ha Tinh </span> Province.",
+    "Youtuber at <span class='font-semibold'>@zhao.</span>",
+    "I am constantly <span class='font-semibold'>learning</span> and <span class='font-semibold'>improving</span> myself everyday."],
+    typeSpeed: 20,
+    backSpeed: 20,
+    backDelay: 1500,
+    loop: true
+});
