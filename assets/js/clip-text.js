02247fd928bc3639de254ac350b5e645a796fba0
Tuan-Dung Bui <dungbuit1k28@gmail.com>
2023-05-04 06:31:04 +0700

    add profile card

--- assets/js/clip-text.js
+++ assets/js/clip-text.js
@@ -1,8 +1,8 @@
 var typed = new Typed(".clip-text", {
-    strings: ["Computer Science Student at <span class='font-semibold'>UET-VNU.</span>", 
+    strings: ["Computer Science Student at <span class='font-semibold'>UET-VNU.</span>",
+    "Youtuber at <span class='font-semibold'>@zhao.</span>", 
     "I live in <span class='font-semibold'>Ha Noi, Viet Nam.</span>", 
     "I was born in the beautiful <span class='font-semibold'>Ha Tinh </span> Province.",
-    "Youtuber at <span class='font-semibold'>@zhao.</span>",
     "I am constantly <span class='font-semibold'>learning</span> and <span class='font-semibold'>improving</span> myself everyday."],
     typeSpeed: 20,
     backSpeed: 20,
