786c2f56bcdf07eb24e6d676e8d151817d5bf423
zhaospei <zhaospei@users.noreply.github.com>
2023-09-10 06:48:05 +0000

    deploy: e0705e0bd44e4a2c8234eaf4f3a6fcafe1ddcb9c

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,47 @@ const systemTheme = window.matchMedia("(prefers-color-scheme:dark)").matches;
 
 const svg = document.querySelector(".svg-change-color");
 
+function getCurrentURL () {
+    return window.location.pathname
+  }
+  
+  // Example
+const url = getCurrentURL();
+
+const title = document.querySelector(".blog-link");
+
+words = url.split('/')
+
+switch (words[1]) {
+    case 'works.html':
+        title.innerHTML = "~/works";
+        break;
+    case 'blog.html':
+        title.innerHTML = "~/blog";
+        break;
+    
+    case 'about':
+        title.innerHTML = "~/about";
+        break;
+    
+    case '':
+        break;
+
+    case 'tag':
+        title.innerHTML = "~/tag/" + words[2].replace('%20', ' ');
+        break;
+
+    default:
+        var cc = document.querySelector(".container h1");
+        if (cc != undefined && cc.innerHTML == '404') {
+            title.innerHTML = "~/404"
+        } else {
+            wo = "~/" + words[1] + "/" + words[words.length - 2];
+            wo = wo.substring(0, 30) + '...';
+            title.innerHTML = wo;
+        }
+}
+
 const iconToggle = () => {
     moonIcon.classList.toggle("hidden");
     sunIcon.classList.toggle("hidden");
